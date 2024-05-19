5bb90b323e32b40a52e2178ff9d442df1fdca975
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2024-05-19 19:08:57 +0700

    update all service

---
+++ ScheduleService/src/main/java/com/microservice/scheduleservice/dtos/ApiResponse.java
@@ -0,0 +1,2 @@
+package com.microservice.scheduleservice.dtos;public class ApiResponse {
+}
---
+++ ScheduleService/src/main/java/com/microservice/scheduleservice/enums/EnrollmentStatus.java
@@ -0,0 +1,16 @@
+package com.microservice.enrollmentservice.enums;
+
+public enum EnrollmentStatus {
+    NEW(0),
+    OLD(1);
+
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    EnrollmentStatus(int value) {
+        this.value = value;
+    }
+}
